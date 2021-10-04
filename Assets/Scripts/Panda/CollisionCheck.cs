9b4fa05c1c2e5962c2ae6cc5d5294a5e8777d454
simone.maccio <simone.maccio@edu.unige.it>
2021-10-04 18:55:42 +0200

    Added collision count, reset count, support for multi-trial

---
+++ Assets/Scripts/Panda/CollisionCheck.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionCheck : MonoBehaviour
+{
+
+    private int collisions;
+
+    private void Start()
+    {
+        collisions = 0;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "robot" || collision.collider.tag == "piece")
+        {
+            collisions += 1;
+        }
+    }
+
+    public int GetCollisions()
+    {
+        return collisions;
+    }
+
+    public void ResetCollisions()
+    {
+        collisions = 0;
+    }
+}
--- Assets/Scripts/Panda/ROSAssemblyInterface.cs
+++ Assets/Scripts/Panda/ROSAssemblyInterface.cs
@@ -23,10 +23,6 @@ public class ROSAssemblyInterface : MonoBehaviour
     private GameObject piece2;
     private GameObject assembled;
 
-    //private List<Transform> pickPoses;
-    private List<GameObject> instantiatedCubes;
-    //private Transform[] placePoses;
-
     // Canvas and interface objects
     public GameObject poseMarker;
     private GameObject[] buttons;
@@ -34,21 +30,33 @@ public class ROSAssemblyInterface : MonoBehaviour
     // Variables required for ROS communication
     public string plannerServiceName = "motion_planner_service";
     public string nextStepTopicName = "next_step";
+    public string stepDoneTopicName = "step_done";
+    public string collisionCountTopicName = "collision_count";
 
     private RobotController controller;
     private RobotController controller_hologram;
+    private bool isHoloActive = false;
 
     // Utility variables
-    private float deltaT = 3.0f;
-
+    private int nChildren;
     private int assemblyStep = 0;
     private int assemblyCount = 0;
     private int stepSolvedCount = 0;
+    private int collisionCount = 0;
     private bool prevStepSolved = true;
-    private Vector3 piece1InitPosition;
-    private Vector3 piece2InitPosition;
-    private Quaternion piece1InitRot;
-    private Quaternion piece2InitRot;
+    private Vector3 markerPosition;
+
+    private List<Vector3> piece1InitPositions;
+    private List<Vector3> piece2InitPositions;
+    private List<Quaternion> piece1InitRotations;
+    private List<Quaternion> piece2InitRotations;
+
+    private Material green;
+    private Material wood;
+
+    //private Vector3[] buttonsOffsets = { new Vector3(-0.6f, 0.65f, 0.0f), new Vector3(0.6f, 0.65f, 0.0f), new Vector3(0.6f, 0.5f, 0.0f) };
+
+    public float deltaT = 10.0f;
 
 
     void Start()
@@ -67,36 +75,64 @@ public class ROSAssemblyInterface : MonoBehaviour
         controller_hologram = gameObject.AddComponent<RobotController>();
         controller_hologram.Init(robot_hologram, ground_holo.transform);
 
+        nChildren = chair.transform.childCount;
+
+        piece1InitPositions = new List<Vector3>();
+        piece2InitPositions = new List<Vector3>();
+        piece1InitRotations = new List<Quaternion>();
+        piece2InitRotations = new List<Quaternion>();
+
+        wood = Resources.Load("Materials/wood") as Material;
+        green = Resources.Load("Materials/green") as Material;
     }
 
     public void Spawn()
     {
-        var markerPosition = poseMarker.transform.position;
+        markerPosition = new Vector3(
+            poseMarker.transform.position.x,
+            poseMarker.transform.position.y, 
+            poseMarker.transform.position.z
+        );
         poseMarker.GetComponent<Behaviour>().enabled = false;
 
         controller.Spawn(markerPosition);
-        controller_hologram.Spawn(markerPosition);
-
+        
         poseMarker.SetActive(false);
     }
 
     private void Dispatcher(Int32 msg)
     {
-        if(msg.data > 0)
+        if(msg.data > 0 && msg.data < 6)
         {
             NextAssemblyStep(msg.data);
             return;
         }
-        else if(msg.data < 0)
+        else if(msg.data > 6)
+        {
+            if(msg.data == 8)
+            {
+                SetHologram(true);
+            }
+            else
+            {
+                SetHologram(false);
+            }
+        }
+        else if(msg.data == -1)
         {
             ResetStep();
+            return;
+        }
+        else if(msg. data == 0)
+        {
+            ResetScene();
             return;
         }
     }
 
     private void NextAssemblyStep(int value)
     {
-        if(assemblyCount < chair.transform.childCount - 1 && prevStepSolved)
+        if(assemblyCount < nChildren - 1 && prevStepSolved)
         {
             assemblyStep += 1;
             if (assemblyStep > 1)
@@ -112,20 +148,21 @@ public class ROSAssemblyInterface : MonoBehaviour
             piece2 = chair.transform.GetChild(assemblyCount + 1).gameObject;
             assembled = chair.transform.GetChild(assemblyCount + 2).gameObject;
 
-            piece1InitPosition = new Vector3(piece1.transform.position.x, piece1.transform.position.y, piece1.transform.position.z);
-            piece2InitPosition = new Vector3(piece2.transform.position.x, piece2.transform.position.y, piece2.transform.position.z);
-            piece1InitRot = new Quaternion(
+            piece1InitPositions.Add(new Vector3(piece1.transform.position.x, piece1.transform.position.y, piece1.transform.position.z));
+            piece2InitPositions.Add(new Vector3(piece2.transform.position.x, piece2.transform.position.y, piece2.transform.position.z));
+
+            piece1InitRotations.Add(new Quaternion(
                 piece1.transform.rotation.x,
                 piece1.transform.rotation.y,
                 piece1.transform.rotation.z,
                 piece1.transform.rotation.w
-            );
-            piece2InitRot = new Quaternion(
+            ));
+            piece2InitRotations.Add(new Quaternion(
                 piece2.transform.rotation.x,
                 piece2.transform.rotation.y,
                 piece2.transform.rotation.z,
                 piece2.transform.rotation.w
-            );
+            ));
 
             piece1.SetActive(true);
             piece2.SetActive(true);
@@ -140,10 +177,6 @@ public class ROSAssemblyInterface : MonoBehaviour
             assembled.SetActive(false);
             assembled = chair.transform.GetChild(chair.transform.childCount - 1).gameObject;
             assembled.SetActive(true);
-            /*foreach(GameObject button in buttons)
-            {
-                button.SetActive(false);
-            }*/
         }
 
         if(value < 5)
@@ -154,11 +187,13 @@ public class ROSAssemblyInterface : MonoBehaviour
 
     private void ResetStep()
     {
-        piece1.transform.SetPositionAndRotation(piece1InitPosition, piece1InitRot);
+        piece1.transform.SetPositionAndRotation(piece1InitPositions[assemblyStep-1], piece1InitRotations[assemblyStep-1]);
         piece1.GetComponent<ObjectManipulator>().enabled = true;
+        piece1.GetComponent<Renderer>().material = wood;
 
-        piece2.transform.SetPositionAndRotation(piece2InitPosition, piece2InitRot);
+        piece2.transform.SetPositionAndRotation(piece2InitPositions[assemblyStep-1], piece2InitRotations[assemblyStep-1]);
         piece2.GetComponent<ObjectManipulator>().enabled = true;
+        piece2.GetComponent<Renderer>().material = wood;
 
         stepSolvedCount = 0;
 
@@ -191,6 +226,7 @@ public class ROSAssemblyInterface : MonoBehaviour
             {
                 piece1.transform.SetPositionAndRotation(childPose.position, childPose.localRotation);
                 piece1.GetComponent<ObjectManipulator>().enabled = false;
+                piece1.GetComponent<Renderer>().material = green;
                 
                 stepSolvedCount += 1;
             }
@@ -219,6 +255,7 @@ public class ROSAssemblyInterface : MonoBehaviour
             {
                 piece2.transform.SetPositionAndRotation(childPose.position, childPose.localRotation);
                 piece2.GetComponent<ObjectManipulator>().enabled = false;
+                piece2.GetComponent<Renderer>().material = green;
 
                 stepSolvedCount += 1;
             }
@@ -228,6 +265,7 @@ public class ROSAssemblyInterface : MonoBehaviour
         {
             stepSolvedCount = 0;
             prevStepSolved = true;
+            ros.Send(stepDoneTopicName, new Bool(data: true));
         }
     }
 
@@ -235,7 +273,6 @@ public class ROSAssemblyInterface : MonoBehaviour
     {
         yield return new WaitForSeconds(1.0f);
         StartCoroutine(PlanAction());
-        stepSolvedCount += 1;
     }
 
     private IEnumerator PlanAction()
@@ -247,7 +284,6 @@ public class ROSAssemblyInterface : MonoBehaviour
         if(rnd == 1)
         {
             piece1.GetComponent<Rigidbody>().isKinematic = false;
-            piece1.GetComponent<ObjectManipulator>().enabled = false;
 
             childPose = assembled.transform.GetChild(2 * (assemblyStep - 1)).transform;
             pickPosition = piece1.transform.localPosition;
@@ -255,7 +291,6 @@ public class ROSAssemblyInterface : MonoBehaviour
         else
         {
             piece2.GetComponent<Rigidbody>().isKinematic = false;
-            piece2.GetComponent<ObjectManipulator>().enabled = false;
 
             childPose = assembled.transform.GetChild(2 * (assemblyStep - 1) + 1).transform;
             pickPosition = piece2.transform.localPosition;
@@ -278,15 +313,37 @@ public class ROSAssemblyInterface : MonoBehaviour
         {
             yield return new WaitForSeconds(0.1f);
         }
-        if(rnd == 1)
+        if(rnd == 1 && (piece1.transform.position - childPose.transform.position).magnitude < 0.05f)
         {
+            
             piece1.transform.SetPositionAndRotation(childPose.position, childPose.localRotation);
             piece1.GetComponent<Rigidbody>().isKinematic = true;
+            piece1.GetComponent<ObjectManipulator>().enabled = false;
+            piece1.GetComponent<Renderer>().material = green;
+            stepSolvedCount += 1;
         }
-        else
+        else if(rnd == 1 && (piece1.transform.position - childPose.transform.position).magnitude >= 0.05f)
+        {
+            piece1.GetComponent<Rigidbody>().isKinematic = true;
+        }
+        else if(rnd == 2 && (piece2.transform.position - childPose.transform.position).magnitude < 0.05f)
         {
             piece2.transform.SetPositionAndRotation(childPose.position, childPose.localRotation);
             piece2.GetComponent<Rigidbody>().isKinematic = true;
+            piece2.GetComponent<ObjectManipulator>().enabled = false;
+            piece2.GetComponent<Renderer>().material = green;
+            stepSolvedCount += 1;
+        }
+        else if(rnd == 2 && (piece2.transform.position - childPose.transform.position).magnitude >= 0.05f)
+        {
+            piece2.GetComponent<Rigidbody>().isKinematic = true;
+        }
+
+        if(stepSolvedCount == 2)
+        {
+            stepSolvedCount = 0;
+            prevStepSolved = true;
+            ros.Send(stepDoneTopicName, new Bool(data: true));
         }
     }
 
@@ -297,8 +354,71 @@ public class ROSAssemblyInterface : MonoBehaviour
 
     private IEnumerator TrajectoryExecutioner(PlannerServiceResponse response)
     {
-        StartCoroutine(controller_hologram.ExecuteTrajectories(response));
+        if(isHoloActive)
+        {
+            StartCoroutine(controller_hologram.ExecuteTrajectories(response));
+            
+        }
         yield return new WaitForSeconds(deltaT);
         StartCoroutine(controller.ExecuteTrajectories(response));
+    }
+
+    private void SetHologram(bool value)
+    {
+        if(!isHoloActive && value)
+        {
+            isHoloActive = true;
+            controller_hologram.Spawn(robot.transform.position);
+        }
+        else if(isHoloActive && !value)
+        {
+            isHoloActive = false;
+            robot_hologram.SetActive(false);
+        }
+        
+    }
+
+    private void ResetScene()
+    {
+        int aux = 0;
+        for(int i=0; i< piece1InitPositions.Count; i++)
+        {
+            var piece1 = chair.transform.GetChild(aux);
+            var piece2 = chair.transform.GetChild(aux+1);
+
+            var c1 = piece1.GetComponent<CollisionCheck>();
+            var c2 = piece2.GetComponent<CollisionCheck>();
+
+            collisionCount += (c1.GetCollisions() + c2.GetCollisions());
+
+            c1.ResetCollisions();
+            c2.ResetCollisions();
+
+            piece1.SetPositionAndRotation(piece1InitPositions[i], piece1InitRotations[i]);
+            piece2.SetPositionAndRotation(piece2InitPositions[i], piece2InitRotations[i]);
+            piece1.GetComponent<ObjectManipulator>().enabled = true;
+            piece2.GetComponent<ObjectManipulator>().enabled = true;
+            piece1.GetComponent<Renderer>().material = wood;
+            piece2.GetComponent<Renderer>().material = wood;
+
+            aux +=3;
+        }
+        ros.Send(collisionCountTopicName, new Int32(data: collisionCount));
+
+        assembled.SetActive(false);
+        for(int i=0; i < assemblyCount; i++) {
+            chair.transform.GetChild(i).gameObject.SetActive(false);
+        }
+
+        assemblyCount = 0;
+        assemblyStep = 0;
+        stepSolvedCount = 0;
+        collisionCount = 0;
+        prevStepSolved = true;
+
+        piece1InitPositions.Clear();
+        piece2InitPositions.Clear();
+        piece1InitRotations.Clear();
+        piece2InitRotations.Clear();
     }
 }
